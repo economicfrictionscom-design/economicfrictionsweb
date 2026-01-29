7853b5f24f5433445205eae2ce76dac607ec9bcf
Devansh <economicfrictions.com@gmail.com>
2026-01-30 00:06:46 +0530

    Force rewrite of config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+﻿import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://economicfrictionscom-design.github.io',
