bc10b2be86d99d6b659890532c157becd7d9115d
economicfrictionscom-design <economicfrictions.com@gmail.com>
2026-01-30 00:20:25 +0530

    Update and rename astro.config.mjs to localhost

--- astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://economicfrictionscom-design.github.io/economicfrictionsweb/',
-  base: '/economicfrictionsweb',
-});
