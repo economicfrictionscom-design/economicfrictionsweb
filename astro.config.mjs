339987c97575c50987066cd23043afedaca11b64
economicfrictionscom-design <economicfrictions.com@gmail.com>
2026-01-30 00:12:43 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://economicfrictionscom-design.github.io',
-  base: 'economicfrictionsweb',
+  site: 'https://economicfrictionscom-design.github.io/economicfrictionsweb',
+  base: '/economicfrictionsweb',
 });
