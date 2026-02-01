d203dc1bc3c886b236424c31a0610e31fcc89a30
Devansh <economicfrictions.com@gmail.com>
2026-02-01 07:11:23 +0530

    Remove trailing slash and fix slug crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    site: 'https://www.economicfrictions.com',
-    base: '',
-    // CRITICAL FIXES FOR GITHUB PAGES 404
-    trailingSlash: 'always', 
-    build: {
-      format: 'directory'
-    },
-    integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://www.economicfrictions.com',
+  base: '/',
+  // Removed trailingSlash: 'always'
+  build: {
+    format: 'directory',
+  },
+  integrations: [mdx(), sitemap(), tailwind()],
 });
