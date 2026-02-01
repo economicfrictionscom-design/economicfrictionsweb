2e28190ebfa7c2b1cd57b2b58f21bbcbcfebfca7
Devansh <economicfrictions.com@gmail.com>
2026-02-01 06:18:48 +0530

    mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    site: 'https://www.economicfrictions.com',
-    base: '',
-    trailingSlash: 'always', // Critical for GitHub Pages directory mapping
-    build: {
-      format: 'directory' // Ensures URLs look like /blog/post/
-    },
-    integrations: [mdx(), sitemap(), tailwind()],
+	site: 'https://www.economicfrictions.com',
+	base: '',
+	integrations: [mdx(), sitemap(), tailwind()],
 });
